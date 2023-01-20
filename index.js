0b48931aaf99cfb0cba702318b0052e4de9e67bb
kacperlNYC <113933880+kacperlNYC@users.noreply.github.com>
2023-01-20 13:17:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,28 +2,45 @@
 const express = require('express');
 const app = express()
 
-const courses = [
-    {id: 1, name: 'Web Dev'},
-    {id: 2, name: 'IT'},
-    {id: 3, name: 'Cybersecurity'}
-]
-
-const genres = [
-    "pop",
-    "hip-hop",
-    "rap",
-    "classical",
-    "rock",
-    "jazz",
-    "blues",
-    "electronic"
-]
+const genres = ["Pop", "Hip-Hop", "Rap", "Classical", "Rock", "Jazz", "Blues", "Electronic"]
 
 const songs = [
-    {id: 1, name:"Name", genre: "pop", year: 2023, month: 1}
+    {id: 1, name:"Name", genre: "pop", year: 2023, month: "January"}
 ]
 
 app.use(express.json())
+
+function getMonth(month) {
+    switch(month) {
+        case 0:
+            return "January";
+        case 1:
+            return "February";
+        case 2:
+            return "March";
+        case 3:
+            return "April";
+        case 4:
+            return "May";
+        case 5:
+            return "June";
+        case 6:
+            return "July";
+        case 7:
+            return "August";
+        case 8:
+            return "September";
+        case 9:
+            return "October";
+        case 10:
+            return "November";
+        case 11:
+            return "December";
+        default:
+            return "Invalid Month"
+    }
+
+}
 
 // GET Request
 app.get('/', (req, res) => {
@@ -38,11 +55,7 @@ app.get('/api/songs', (req, res)=>{
     res.status(200).send(songs);
 })
 
-app.get('/api/songs/id', (req, res)=>{
-    res.status(404).send('ID Missing')
-})
-
-app.get('/api/songs/id/:id', (req,res)=> {
+app.get('/api/songs/:id', (req,res)=> {
     let song = songs.find(s=> s.id === parseInt(req.params.id))
     if (!song) {
         res.status(404).send('The song was not found')
@@ -51,11 +64,7 @@ app.get('/api/songs/id/:id', (req,res)=> {
     res.status(200).send(song);
 })
 
-app.get('/api/songs/date', (req,res) => {
-    res.status(404).send("Year and Month not found")
-})
-
-app.get('/api/songs/date/:year/:month', (req,res) => {
+app.get('/api/songs/:year/:month', (req,res) => {
     let filtered = songs.filter((s) => s.year == req.params.year && s.month == req.params.month)
     if (filtered.length === 0) {
         res.status(404).send("No songs found")
@@ -64,7 +73,7 @@ app.get('/api/songs/date/:year/:month', (req,res) => {
     res.status(200).send(filtered)
 })
 
-app.get('/api/songs/genre/:genre', (req,res) => {
+app.get('/api/genre/:genre', (req,res) => {
     let filtered = songs.filter((s) => s.genre == req.params.genre)
     if (filtered.length === 0) {
         res.status(404).send("No songs found")
@@ -94,7 +103,7 @@ app.post('/api/songs', (req,res) => {
     {
         date = new Date()
         year = date.getFullYear()
-        month = date.getMonth()
+        month = getMonth(date.getMonth())
     }
 
     let song = {
@@ -110,9 +119,7 @@ app.post('/api/songs', (req,res) => {
     res.status(200).send(song)
 });
 
-// TODO: add genre post req
-
-app.put('/api/courses/:id', (req,res)=>{
+app.put('/api/songs/:id', (req,res)=>{
     let song = songs.find((s) => s.id === parseInt(req.params.id))
     if (!song) {
         res.status(404).send('The song was not found')
@@ -128,30 +135,30 @@ app.put('/api/courses/:id', (req,res)=>{
         res.status(404).send('Invalid genre')
     }
     else {
-        songs["name"] = req.body.name
-        songs["genre"] = req.body.genre;
+        song["name"] = req.body.name
+        song["genre"] = req.body.genre;
         // TODO: add date?
         res.status(200).send(song)
     }
 });
     
-app.delete('/api/courses/:id', (req,res)=>{
-    course = courses.find(c=> c.id === parseInt(req.params.id))
-    if (!course)
+app.delete('/api/songs/:id', (req,res)=>{
+    let song = songs.find(s => s.id === parseInt(req.params.id))
+    if (!song)
     {
-        res.status(404).send('The course was not found')
+        res.status(404).send('The song was not found')
         return
     }
     else
     {
-        let index = courses.indexOf(course)
-        courses.splice(index, index+1)
-        res.status(200).send(course)
+        let index = songs.indexOf(song)
+        songs.splice(index, index+1)
+        res.status(200).send(song)
 
     }
 });
 
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000 ..")
+    console.log("Listening on port 3000...")
 })
